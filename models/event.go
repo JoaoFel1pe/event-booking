6264c033d585507c142ab00bcd11090c0e6512ee
joao.felipe <joao.felipe@propig.com.br>
2024-05-25 18:53:03 -0300

    [ db ] -> Storing Data in the Database (INSERT)

--- models/event.go
+++ models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"event-booking-api/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,10 +16,27 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
+func (e Event) Save() error {
 	// add it to a database later
 	events = append(events, e)
+	query := `
+	INSERT INTO events(name, description, location, date_time, user_id)
+	VALUES (?, ?, ?, ?, ?)`
 
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
 func GetAllEvents() []Event {
