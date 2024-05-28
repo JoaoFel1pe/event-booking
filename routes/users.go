4c6955f81a398fb76967cfcb6c11641991d0f014
joao.felipe <joao.felipe@propig.com.br>
2024-05-28 13:10:38 -0300

    [ feat ] -> Adding Login Logic

--- routes/users.go
+++ routes/users.go
@@ -23,3 +23,20 @@ func signup(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "user created successfully"})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "could not authenticate user."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successfull"})
+}
