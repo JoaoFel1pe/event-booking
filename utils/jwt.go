ee75a85c53c3c4151358f25892803d08a0ea31e9
joao.felipe <joao.felipe@propig.com.br>
2024-06-03 20:25:19 -0300

    [ build ] -> Removing Static Database & Secret Variables

--- utils/jwt.go
+++ utils/jwt.go
@@ -2,12 +2,13 @@ package utils
 
 import (
 	"errors"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-const secretKey = "ababa"
+var secretKey string = os.Getenv("SECRET_KEY")
 
 func GenerateToken(email string, userId int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
